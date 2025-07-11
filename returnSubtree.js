fcf9f859f11161f1794ee9822560f5499b1f358f
fahar-commits <faharahmad509@gmail.com>
2025-07-11 10:21:49 +0530

    Add 3 more Neetcode problems solved

---
+++ returnSubtree.js
@@ -0,0 +1,15 @@
+function isSubtree(mainTree,subtree) {
+    function isSameTree(p,q) {
+        if (!p && !q) {
+            return true
+        }
+        if (!p || !q) {
+            return false
+        }
+        if (p.val !== q.val) {
+            return false
+        }
+
+        
+    }
+}
